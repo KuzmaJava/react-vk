7f16e49fddeccd6c6e36e5011886af56bcc3e104
KuzmaJava <zheniakuzma@gmail.com>
2024-01-13 19:00:31 +0200

    Added basic layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+    return <header className='header'>
+        <img
+            src='https://media.istockphoto.com/id/1313644269/vector/gold-and-silver-circle-star-logo-template.jpg?s=612x612&w=0&k=20&c=hDqCI9qTkNqNcKa6XS7aBim7xKz8cZbnm80Z_xiU2DI='/>
+    </header>
+}
+
+export default Header;
