ddf0c65482f11143a9ed7830d132c3f35be2e7d3
KuzmaJava <zheniakuzma@gmail.com>
2024-01-14 21:15:57 +0200

    Added css modules

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,24 +1,27 @@
 import React from 'react'
+import styles from './Profile.module.css';
 
 const Profile = () => {
-    return <div className='content'>
+    return <div className={styles.content}>
         <div>
             <img
                 src='https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW1hZ2V8ZW58MHx8MHx8fDA%3D'/>
         </div>
         <div>
-            <img src='https://cdn.pixabay.com/photo/2017/01/03/09/18/woman-1948939_1280.jpg'/>
+            <img src='https://discoverymood.com/wp-content/uploads/2020/04/Mental-Strong-Women-min.jpg'/>
         </div>
         <div>
             My posts
             <div>
                 New post
             </div>
-            <div>
-                post 1
-            </div>
-            <div>
-                post 2
+            <div className='posts'>
+                <div className={styles.item}>
+                    post 1
+                </div>
+                <div className={styles.item}>
+                    post 2
+                </div>
             </div>
         </div>
     </div>
