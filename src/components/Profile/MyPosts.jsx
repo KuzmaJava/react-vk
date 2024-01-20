aea5b7bc203f856dc34b2151e579e6ecc24436ce
KuzmaJava <zheniakuzma@gmail.com>
2024-01-20 20:22:13 +0200

    Убрали амендик?

--- src/components/Profile/MyPosts.jsx
+++ src/components/Profile/MyPosts.jsx
@@ -16,6 +16,7 @@ const MyPosts = () => {
             <Post message="I've bought the car"/>
             <Post message="That's my first post"/>
             <Post message="One more post" />
+            <Post message="убрать амендик" />
         </div>
     </div>
 }
