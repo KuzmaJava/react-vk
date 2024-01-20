ae1f04fae38a46abfd7e3eeefce5b01b2af92a6c
KuzmaJava <zheniakuzma@gmail.com>
2024-01-20 22:19:44 +0200

    UI - BLL Extracted arrays data into abstract variables

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -13,18 +13,32 @@ const Message = (props) => {
 }
 
 const Dialogs = (props) => {
+
+    let dialogsData = [
+        {id: 1, name: 'Jackson'},
+        {id: 2, name: 'Maria'},
+        {id: 3, name: 'Robert'}
+    ]
+
+    let messagesData = [
+        {id: 1, message: 'Hey brother'},
+        {id: 2, message: 'How are you do in man'},
+        {id: 3, message: 'See yo'},
+    ]
+
     return (
         <div>
             <div className={styles.dialogs}>
                 <div className={styles.dialogsItems}>
-                    <DialogItem name="Jackson" id="1"/>
-                    <DialogItem name="Maria" id="2"/>
-                    <DialogItem name="Robert" id="3"/>
+                    <DialogItem name={dialogsData[0].name} id={dialogsData[0].id}/>
+                    <DialogItem name={dialogsData[1].name} id={dialogsData[1].id}/>
+                    <DialogItem name={dialogsData[2].name} id={dialogsData[2].id}/>
+
                 </div>
                 <div className={styles.messages}>
-                    <Message message="Hey brother"/>
-                    <Message message="How are you do in man"/>
-                    <Message message="See yo"/>
+                    <Message message={messagesData[0].message}/>
+                    <Message message={messagesData[1].message}/>
+                    <Message message={messagesData[2].message}/>
                 </div>
             </div>
         </div>
