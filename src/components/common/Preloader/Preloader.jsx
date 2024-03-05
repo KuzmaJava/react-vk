1975e42a3ad8f77939779d4527767cfb9afb282e
KuzmaJava <zheniakuzma@gmail.com>
2024-03-05 21:07:20 +0200

    Added Preloader component.

---
+++ src/components/common/Preloader/Preloader.jsx
@@ -0,0 +1,10 @@
+import preLoader from "../../../resources/images/pre_loader.gif";
+import React from "react";
+
+let Preloader = (props) => {
+    return <div>
+        <img src={preLoader}/>
+    </div>
+}
+
+export default Preloader;
