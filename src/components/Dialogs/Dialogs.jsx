f85c0fcc9d953a7cf0ec1c02cc80e02e41845b59
KuzmaJava <zheniakuzma@gmail.com>
2024-03-18 10:53:52 +0200

    Improved isAuth statement.

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -25,7 +25,7 @@ const Dialogs = (props) => {
         props.updateNewMessageBody(body);
     }
 
-    if (props.isAuth === false) return <Navigate to={"/login"} />;
+    if (!props.isAuth) return <Navigate to={"/login"} />;
 
     return (
         <div>
