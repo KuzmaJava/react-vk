3a1af1157b3aece5be09426a9ba211f22760aa02
KuzmaJava <zheniakuzma@gmail.com>
2024-04-11 17:51:53 +0300

    Added React.Lazy and React.Suspense usage.

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 import Navbar from "./components/Navbar/Navbar";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profile/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import LoginPage from "./components/Login/LoginPage";
 import {connect} from "react-redux";
 import {compose} from "redux";
 import {initializeApp} from "./redux/app-reducer";
 import Preloader from "./components/common/Preloader/Preloader";
+import {withSuspense} from "./hoc/withSuspense";
+
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
+
 
 class App extends React.Component {
     componentDidMount() {
@@ -30,9 +33,15 @@ class App extends React.Component {
                     <Navbar/>
                     <div className='app-wrapper-content'>
                         <Routes>
-                            <Route path='/dialogs' element={<DialogsContainer/>}/>
-                            <Route path='/profile' element={<ProfileContainer />} />
-                            <Route path='/profile/:userId' element={<ProfileContainer/>}/>
+                            <Route path='/dialogs' element={
+                                withSuspense(DialogsContainer)
+                            }/>
+                            <Route path='/profile' element={
+                                withSuspense(ProfileContainer)
+                            }/>
+                            <Route path='/profile/:userId' element={
+                                withSuspense(ProfileContainer)
+                            }/>
                             <Route path='/users' element={<UsersContainer/>}/>
                             <Route path='/login' element={<LoginPage/>}/>
                         </Routes>
---
+++ src/hoc/withSuspense.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export const withSuspense = (Component) => {
+    return <React.Suspense fallback={<div>Loading...</div>}>
+        <Component/>
+    </React.Suspense>
+}
